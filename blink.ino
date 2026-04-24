2353ce39d8498eaaf6a526ac9375dcaca1843c20
ianyamauti <ian.yamauti@usp.br>
2026-04-24 10:27:27 -0300

    feat: atualizar semaforo no blink.ino

--- blink.ino
+++ blink.ino
@@ -25,16 +25,23 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(10, OUTPUT);
   pinMode(8, OUTPUT);
+  digitalWrite(13, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(8, LOW);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(8, LOW);
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(10, HIGH);
+  delay(1000);
+  digitalWrite(10, LOW);
+  digitalWrite(13, HIGH);
+  delay(3000);
+  digitalWrite(13, LOW);
   digitalWrite(8, HIGH);
-  delay(1000);                       // wait for a second
+  delay(2000);
 }
